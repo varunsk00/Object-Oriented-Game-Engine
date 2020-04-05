ea1f53dced9edebf2fa487fa9ffea5719459710c
Shruthi Kumar <sk593@duke.edu>
2020-04-05 18:51:13 -0400

    added action factory

--- src/ooga/model/actions/Action.java
+++ src/ooga/model/actions/Action.java
@@ -1,5 +1,43 @@
 package ooga.model.actions;
 
-public abstract class Action {
-  public abstract void execute();
+import java.util.Collection;
+import java.util.List;
+import java.util.Stack;
+import ooga.model.EntityModel;
+
+public  interface Action {
+  /**
+   *
+   *
+   * @param entityModel entity model
+   * @return
+   */
+  public Action execute(EntityModel entityModel);
+
+  /**
+   *
+   */
+  public double returnValue();
+
+  /**
+   * Returns a boolean for whether or not the screen's pen drawings should be cleared.
+   *
+   * @return a boolean, which should be false for all cases except ClearScreen or similar commands.
+   */
+  public static boolean toClear() {
+    return false;
+  }
+
+  /**
+   *
+   *
+   * @param action
+   * @param entityModel
+   * @param ret
+   * @return
+   */
+  static double executeAndExtractValue(Action action, EntityModel entityModel, Stack<Action> ret) {
+    ret.addAll((Collection<? extends Action>) action.execute(entityModel));
+    return action.returnValue();
+  }
 }
--- src/ooga/model/actions/ActionFactory.java
+++ src/ooga/model/actions/ActionFactory.java
@@ -1,5 +1,49 @@
 package ooga.model.actions;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Stack;
+
 public class ActionFactory {
+  private static final double X_MAX = 500; //TODO: set later
+  private static final double Y_MAX = 250;
+
+  public ActionFactory() {
+    setGeneralCommands();
+    setMovementCommands();
+    setControlCommands();
+  }
+
+  public Action makeCommand(String command, Stack<Command> previous, Stack<List<Command>> listCommands, Map<String, List<String>> myCommands) throws InvalidArgumentException{
+    String formalCommand = validateCommand(command, myCommands);
+
+    List<Command> commands = new ArrayList<>();
+    int count = getCount(formalCommand);
+
+    if(previous.size() + listCommands.size() < count){ //TODO: TYLER EDITED
+      throw new InvalidArgumentException(String.format("Incorrect number of arguments for command %s", command));
+    }
+    if (formalCommand.equals("Tell")) {
+      while (commands.size() + listCommands.size() < count) {
+        if (previous.size() > 0) {
+          commands.add(previous.pop());
+        } else {
+          break;
+        }
+      }
+    } else {
+      while (commands.size() < count) {
+        if (previous.size() > 0) {
+          commands.add(previous.pop());
+        } else {
+          break;
+        }
+      }
+    }
+    return buildCommand(formalCommand, commands, listCommands);
+  }
+
+
 
 }
