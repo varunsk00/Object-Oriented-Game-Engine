30a57e184f2a8604cff068f08f9653bee7d80afe
Shruthi Kumar <sk593@duke.edu>
2020-04-06 00:35:32 -0400

    working on action factory reflection

--- src/ooga/model/actions/ActionFactory.java
+++ src/ooga/model/actions/ActionFactory.java
@@ -1,35 +1,40 @@
 package ooga.model.actions;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.ResourceBundle;
 import java.util.Stack;
 
 public class ActionFactory {
 
   private static final double X_MAX = 500; //TODO: set later
   private static final double Y_MAX = 250;
+  private Map<String, String> myActions = new HashMap<>();
+  private ResourceBundle actionBundle;
 
   public ActionFactory() {
-    /*setGeneralCommands();
+    setGeneralCommands();
+
+    /*
     setMovementCommands();
     setControlCommands();
-
      */
   }
 
-  public Action makeAction(String action, Stack<Action> previous, Stack<List<Action>> listActions,
-      Map<String, List<String>> myCommands) {// throws InvalidArgumentException{
-    //String formalCommand = validateAction(command, myCommands);
-
-    List<Action> commands = new ArrayList<>();
-//    int count = getCount(formalCommand);
-
-    if (previous.size() + listActions.size() < 0) { //TODO: fix count var){
-      //throw new InvalidArgumentException(String.format("Incorrect number of arguments for command %s", command));
+  private void setGeneralCommands() {
+    var actionBundle = ResourceBundle.getBundle(ActionFactory.class.getPackageName() + "resources." + "actions");
+    for(String tag : actionBundle.keySet()) {
+      myActions.put(tag, actionBundle.getString(tag));
     }
-    return null;
+  }
 
+  public Action makeAction(String action, Stack<Action> previous, Stack<List<Action>> listActions,
+      Map<String, List<String>> myActions) {
+    String formalAction = validateAction(action, myActions);
+
+    return buildAction(formalAction);
   }
 }
 
