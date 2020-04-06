ae616deade9904e709709afcf07a2eb72f1a879e
Varun <varun.kosgi@duke.edu>
2020-04-06 17:47:47 -0500

    removed unnecessary lines

--- src/ooga/view/gui/GameCabinet.java
+++ src/ooga/view/gui/GameCabinet.java
@@ -1,25 +1,19 @@
 package ooga.view.gui;
+import ooga.view.application.TestSandbox;
+
 
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.application.Application;
-import javafx.geometry.Insets;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.util.Duration;
-import ooga.view.application.TestSandbox;
-
-import java.io.IOException;
-import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.ResourceBundle;
 
 public class GameCabinet extends Application {
-    private Scene myScene;
     private static final int SCENE_WIDTH = 1280;
     private static final int SCENE_HEIGHT = 720;
     private static final double FRAMES_PER_SECOND = 30;
@@ -27,12 +21,8 @@ public class GameCabinet extends Application {
     private GameSelectionMenu myGameCabinet;
     private List<GamePreview> myGames;
     private Timeline animation;
-    private ResourceBundle myResources;
-    private HBox customizationPanel = new HBox();
-    private BorderPane mainView = new BorderPane();
     private BorderPane mainFrame = new BorderPane();
     private Stage myStage;
-    String selectedGame;
 
     public GameCabinet() { }
 
@@ -40,14 +30,12 @@ public class GameCabinet extends Application {
         launch(args);
     }
 
-
     public void start(Stage primaryStage) {
         primaryStage.setTitle("BOOGA");
         myStage = primaryStage;
         startAnimationLoop();
         initGameSelect();
-        setupUI();
-        handleGameSelect(myStage);
+        mainFrame.setCenter(myGameCabinet);
         Scene scene = new Scene(mainFrame, SCENE_WIDTH, SCENE_HEIGHT);
         myStage.setScene(scene);
         myStage.show();
@@ -65,24 +53,7 @@ public class GameCabinet extends Application {
         updateCurrentGame();
     }
 
-    private void updateCurrentGame(){
-        for(GamePreview game: myGames){
-            if(game.getGameName() != null) {
-                if(game.getGameName().equals("0x008000ff")) {
-                    game.resetGameName();
-                    new TestSandbox(myStage);
-                    myStage.setTitle("TestSandbox");
-                    //myStage.setScene(new TestSandbox(myStage));
-                }
-            }
-        }
-    }
-
-    public String getSelectedGame(){
-        return selectedGame;
-    }
-
-    private void initGameSelect(){
+    private void initGameSelect(){ //FIXME: STREAMLINE INSTANTIATION TO READ FROM A FILE
         this.myGames = new ArrayList<>();
         GamePreview g1 = new GamePreview(Color.BLUE);
         GamePreview g2 = new GamePreview(Color.RED);
@@ -93,19 +64,15 @@ public class GameCabinet extends Application {
         this.myGameCabinet = new GameSelectionMenu(myGames);
     }
 
-    public void setupUI() {
-        mainFrame.setCenter(myGameCabinet);
-    }
-
-    private void handleGameSelect(Stage primaryStage) {
+    private void updateCurrentGame(){
         for(GamePreview game: myGames){
-//            if(!game.getGameName().equals(null)){
-//                System.out.println("YEET");
-//            }
+            if(game.getGameName() != null) {
+                if(game.getGameName().equals("0x008000ff")) {
+                    game.resetGameName();
+                    new TestSandbox(myStage);
+                    myStage.setTitle("TestSandbox");
+                }
+            }
         }
-//        if (selectedGame.equals("0x008000ff")){
-//            System.out.println("YEET");
-//        }
     }
-
 }
