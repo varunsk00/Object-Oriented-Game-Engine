bb9ace42fc8abf7e4d84aa046ac5826e3254460c
Frank Tang <ft39@duke.edu>
2020-04-09 04:35:44 -0400

    Added collision detection with the sides of collided entities; currently has functionality up to returning the side of the targetEntity so that we can use the CollisionKey to instantiate an action with the CollisionMap.

--- src/ooga/model/CollisionEngine.java
+++ src/ooga/model/CollisionEngine.java
@@ -1,8 +1,29 @@
 package ooga.model;
 
 
+import java.sql.Array;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import javafx.util.Pair;
 
 public class CollisionEngine {
+
+  private static final int ZERO = 0;
+  private static final int ONE = 1;
+  private static final int TWO = 2;
+  private static final int THREE = 3;
+  private static final String LEFT = "Left";
+  private static final String RIGHT = "Right";
+  private static final String TOP = "Top";
+  private static final String BOTTOM = "Bottom";
+
+  private static final Map<Integer, String> targetEntitySideMap = Map.ofEntries(
+      Map.entry(ZERO, RIGHT),
+      Map.entry(ONE, LEFT),
+      Map.entry(TWO, BOTTOM),
+      Map.entry(THREE, TOP)
+      );
 
   public CollisionEngine() {
     //TODO: do I need a parameter to the constructor?
@@ -10,12 +31,32 @@ public class CollisionEngine {
 
   public void produceCollisionActions(EntityModel subjectEntity, EntityModel targetEntity) {
     if(!subjectEntity.equals(targetEntity) && detectCollision(subjectEntity, targetEntity)){
-      String collisionSide = determineCollisionSide(subjectEntity, targetEntity);
+      String targetEntityCollisionSide = determineTargetEntityCollisionSide(subjectEntity, targetEntity);
+
+
     }
   }
 
-  private String determineCollisionSide(EntityModel subjectEntity, EntityModel targetEntity){
-    return "sTreing";
+  private String determineTargetEntityCollisionSide(EntityModel subjectEntity, EntityModel targetEntity){
+    double[] subjectLeftRightTopBottom = new double[]{subjectEntity.getX(), subjectEntity.getX() + subjectEntity.getWidth(), subjectEntity.getY(), subjectEntity.getY() + subjectEntity.getHeight()};
+    double[] targetRightLeftBottomTop = new double[]{targetEntity.getX() + targetEntity.getWidth(), targetEntity.getX(), targetEntity.getY() + targetEntity.getHeight(), targetEntity.getY()};
+    int sideIndex = determineSideIndex(subjectLeftRightTopBottom, targetRightLeftBottomTop);
+
+    return targetEntitySideMap.get(sideIndex);
+  }
+
+  private int determineSideIndex(double[] subjectSides, double[] targetSides){
+    int sideIndex = ZERO;
+    double minimumDistance = subjectSides[ZERO] * targetSides[ZERO];
+
+    for(int i = 0; i < subjectSides.length; i++){
+        double currentDistance = subjectSides[i] * targetSides[i];
+        if(currentDistance < minimumDistance){
+          minimumDistance = currentDistance;
+          sideIndex = i;
+      }
+    }
+    return sideIndex;
   }
 
   private boolean detectCollision(EntityModel subjectEntity, EntityModel targetEntity){
