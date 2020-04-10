e3b2c87ca8fab79eb189581e4e0145a42173b6c6
Varun <varun.kosgi@duke.edu>
2020-04-10 03:55:44 -0500

    added flappy jump

--- src/ooga/controller/BlueController.java
+++ src/ooga/controller/BlueController.java
@@ -46,7 +46,7 @@ public class BlueController implements Controller {
         entityBuffer = new ArrayList<>();
         entityList.add(new EntityWrapper("Flappy_Bird", this));
 
-        this.camera = new Camera(stageManager.getStage(), level, entityList.get(0).getRender());
+        myViewManager.setUpCamera(entityList.get(0).getRender());
 
         entityWrapper = entityList.get(0);
         myViewManager.updateEntityGroup(entityWrapper.getRender());
