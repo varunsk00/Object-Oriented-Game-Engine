b9a13ca7e1252ff928c458875f02419ea08dc702
Frank Tang <ft39@duke.edu>
2020-04-17 17:11:28 -0400

    Switch to Actions that set the next level advancement variables . Need to still fix spawning only one new level at a time.

---
+++ src/ooga/model/levels/FiniteLevel.java
@@ -0,0 +1,43 @@
+package ooga.model.levels;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.text.html.parser.Entity;
+import ooga.controller.EntityWrapper;
+import ooga.controller.ViewManager;
+import ooga.model.EntityModel;
+
+public class FiniteLevel extends Level{
+
+  private List<EntityWrapper> tileEntities;
+  private List<EntityWrapper> playerEntities;
+  private List<EntityWrapper> enemyEntities;
+
+  public FiniteLevel(List<EntityWrapper> tileList, List<EntityWrapper> playerList, List<EntityWrapper> enemyList){
+    super(tileList, playerList, enemyList);
+    tileEntities = tileList;
+    playerEntities = playerList;
+    enemyEntities = enemyList;
+  }
+
+
+  @Override
+  public void spawnEntities(List<EntityWrapper> currentEntityList, ViewManager viewManager){
+    for (EntityWrapper player : playerEntities) {
+      for (EntityWrapper tileEntity : tileEntities) {
+        if (!playerEntities.contains(tileEntity) && isInRange(player.getModel(), tileEntity.getModel()) && !currentEntityList.contains(tileEntity)) {
+          currentEntityList.add(tileEntity);
+          viewManager.updateEntityGroup(tileEntity.getRender());
+        }
+      }
+      for (EntityWrapper enemyEntity : enemyEntities) {
+        if (!playerEntities.contains(enemyEntity) && isInRange(player.getModel(), enemyEntity.getModel()) && !currentEntityList.contains(enemyEntity)) {
+          currentEntityList.add(enemyEntity);
+          viewManager.updateEntityGroup(enemyEntity.getRender());
+        }
+      }
+    }
+  }
+
+
+}
