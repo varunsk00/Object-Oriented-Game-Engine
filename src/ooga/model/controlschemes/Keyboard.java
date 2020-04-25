7cfb488b78d650612f9162bf7e9a27f38fd0b364
Alex Oesterling <axo@duke.edu>
2020-04-25 00:01:30 -0700

    refactored controlschemes package

--- src/ooga/model/controlschemes/Keyboard.java
+++ src/ooga/model/controlschemes/Keyboard.java
@@ -10,12 +10,13 @@ import ooga.model.actions.Action;
 import ooga.util.ActionBundle;
 
 public class Keyboard extends ControlScheme {
+
   private Map<String, List<Action>> keyBindings;
 
-  public Keyboard(List<ActionBundle> bindings){
+  public Keyboard(List<ActionBundle> bindings) {
     super(bindings);
     keyBindings = new HashMap<>();
-    for(ActionBundle bundle : actionMap){
+    for (ActionBundle bundle : actionMap) {
       keyBindings.put(bundle.getId(), bundle.getActions());
     }
     currentAction = new ArrayList<>();
@@ -28,7 +29,7 @@ public class Keyboard extends ControlScheme {
 
   @Override
   public void handleKeyInput(String key) {
-    if(keyBindings.containsKey(key) && !currentAction.containsAll(keyBindings.get(key))) {
+    if (keyBindings.containsKey(key) && !currentAction.containsAll(keyBindings.get(key))) {
       currentAction.addAll(keyBindings.get(key));
       System.out.println(currentAction);
     }
@@ -39,7 +40,6 @@ public class Keyboard extends ControlScheme {
     if (keyBindings.containsKey(key) && currentAction
         .containsAll(keyBindings.get(key))) {
       currentAction.removeAll(keyBindings.get(key));
-      //System.out.println(keyEvent.getCode().toString());
     }
   }
 }
